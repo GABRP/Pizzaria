878105d0eda0e0e2cbe2ca319a260acad036d764
Gabriel da Costa Rodrigues <112523344+GABRP@users.noreply.github.com>
2023-03-16 23:06:20 -0300

    feat: finalizado!

---
+++ src/br/com/newtonpaiva/Pizza.java
@@ -0,0 +1,14 @@
+package br.com.newtonpaiva;
+
+public class Pizza {
+    String nome;
+    Double valor;
+    Boolean possuiBorda;
+
+    public Double calcular(Double acres){
+        if (possuiBorda = true)
+            acres += 5.00;
+
+        return acres;
+    }
+}
